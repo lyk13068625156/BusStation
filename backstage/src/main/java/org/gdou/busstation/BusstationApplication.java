45d7b52523c674651d411246911bd10e6f9dbaf2
532502394 <532502394@qq.com>
2018-12-24 17:35:05 +0800

    完成核心目录架构、远程数据库连接

---
+++ backstage/src/main/java/org/gdou/busstation/BusstationApplication.java
@@ -0,0 +1,14 @@
+package org.gdou.busstation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusstationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BusstationApplication.class, args);
+    }
+
+}
+
