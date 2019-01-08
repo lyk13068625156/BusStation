5a5ea7c67bf4a16e2b159285e478fca1a38737d7
532502394 <532502394@qq.com>
2019-01-08 15:41:07 +0800

    1-8 bootstrap-table实现：服务端分页（自定义查询未实现）

--- backstage/src/main/java/org/gdou/busstation/service/impl/UserServiceImpl.java
+++ backstage/src/main/java/org/gdou/busstation/service/impl/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package org.gdou.busstation.service.impl;
 
 import org.gdou.busstation.mapper.UserMapper;
+import org.gdou.busstation.model.PageModel;
 import org.gdou.busstation.model.User;
 import org.gdou.busstation.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,7 +16,21 @@ public class UserServiceImpl implements IUserService {
     private UserMapper userMapper;
 
     @Override
-    public List<User> getAllUser() {
-        return userMapper.getAllUser();
+    public PageModel getAllUser(Integer pageNumber, Integer pageSize) {
+        //实例化部分参数
+        PageModel pageModel = new PageModel();
+        pageModel.setPageNumber(pageNumber);
+        pageModel.setPageSize(pageSize);
+
+        //生成数据库所需startIndex，获取数据，获取总数
+        int startIndex = (pageNumber - 1) * pageSize;
+        List<User> userList = userMapper.getAllUser(startIndex, pageSize);
+        int total = userMapper.count();
+
+        //赋值总数、查询结果
+        pageModel.setRows(userList);
+        pageModel.setTotal(total);
+
+        return pageModel;
     }
 }
